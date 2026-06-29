385e681a9cabb206a6158945961a078a792201aa
oolovou <126276483+oolovou@users.noreply.github.com>
2026-06-29 09:48:54 +0800

    更新 NetworkSecurityCheck2.js

--- Script/NetworkSecurityCheck2.js
+++ Script/NetworkSecurityCheck2.js
@@ -1,4 +1,4 @@
-/* 来自群友@BP999X @Aston无私分享
+/* 来自群友@BP999X @Aston无私分享，AI修改。
  * Egern widget: Network Security Check (simplified, no rule stats)
  *
  * Create a Generic script with this file, then add it in Widget Gallery.
